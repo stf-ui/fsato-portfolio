6e809521d691b651c4200d9f68277d2e010b0916
Fumi Sato <mogumogu2382@gmail.com>
2023-02-15 00:00:07 +0900

    設定：公開設定変更

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/fsato-portfolio', //リポジトリ名
+  outputDir: 'docs' //docs
 })
