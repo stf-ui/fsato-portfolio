ce8e318815bdd5d832f4887793080f983191b061
Fumi Sato <mogumogu2382@gmail.com>
2023-02-16 17:20:34 +0900

    meta：調整

--- router/index.js
+++ router/index.js
@@ -10,36 +10,35 @@ const routes = [
     path: '/',
     name: 'topPage',
     component: topPage,
-    meta: { title: 'ページタイトル1' }
+    meta: { title: "Fumi Sato's portfolio - TOP | f.Sato" }
   },
   {
     path: '/profile',
     name: 'profilePage',
-    // component: profilePage,
-    component: () => import(/* webpackChunkName: "about" */ '../src/components/profilePage.vue'),
-    meta: { title: 'About' }
+    component: profilePage,
+    meta: { title: "Fumi Sato's portfolio - PROFILE | f.Sato", desc: 'ディスクリプションを記述' }
   },
   {
     path: '/skill',
     name: 'skillPage',
     component: skillPage,
-    meta: { title: 'ページタイトル3' }
+    meta: { title: "Fumi Sato's portfolio - SKILL | f.Sato" }
   }
   ,
   {
     path: '/work',
     name: 'workPage',
     component: workPage,
-    meta: { title: 'ページタイトル4' }
+    meta: { title: "Fumi Sato's portfolio - WORK | f.Sato" }
   },
   {
     path: '/contact',
     name: 'contactPage',
     component: contactPage,
-    meta: { title: 'ページタイトル5'}
+    meta: {  title: "Fumi Sato's portfolio - CONTACT | f.Sato" }
   }
 ]
-const DEFAULT_TITLE = 'Default Title'
+const DEFAULT_TITLE = "Fumi Sato's portfolio | f.Sato";
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,29 @@ export default {
   components: {
     //使用するコンポーネントの指定
     menuList
+  },
+  methods : {
+    setMeta(route){
+      // タイトルを設定
+      if(route.meta.title){
+        let setTitle = route.meta.title;
+        document.title = setTitle;
+      }
+      // ディスクリプションを設定
+      if(route.meta.desc){
+        let setDesc = route.meta.desc;
+        document.querySelector("meta[name='description']").setAttribute('content', setDesc)
+      }
+    }
+  },
+  mounted(){
+    let route = this.$route;
+    this.setMeta(route);
+  },
+  watch: { 
+    '$route' (route, from) {
+      this.setMeta(route);
+    }
   }
 }
 </script>
